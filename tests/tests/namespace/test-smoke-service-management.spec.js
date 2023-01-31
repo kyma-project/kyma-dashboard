baaa95ebd4a14e060d79a8a32eb69c8b59d18a8b
Jakub Lyczko <jakub.lyczko1@gmail.com>
2023-01-31 10:39:19 +0100

    feat: add ServiceInstance and ServiceBindings smoke tests (#99)
    * test: wip add tests for service management category
    
    * test: link ServiceInstance and Bindings to test directory
    
    * test: add Service Management tests
    
    * test: fix file name
    
    * test: check whether it works
    
    * test: check whether it works v2
    
    * now?
    
    * import useCategory
    
    * change the order of tests
    
    * check if it works
    
    * try to fix something:
    |
    
    * remove 2 lines
    
    * test: adjust CRDs
    
    * change expect 2 instead of 4
    
    * maybe now?
    
    * maybe now?
    
    * test
    
    * test
    
    * test
    
    * cleanup
    
    * bring back previous version
    
    * fix: remove semlink to service-management extensions
    
    * refactor: change the name of the test file
    
    ---------
    
    Co-authored-by: mrCherry97 <mwisnia97@gmail.com>

---
+++ tests/tests/namespace/test-smoke-service-management.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+import 'cypress-file-upload';
+import jsyaml from 'js-yaml';
+import { useCategory } from '../../support/helpers';
+
+context('Test Service Management category', () => {
+  Cypress.skipAfterFail();
+
+  before(()=>{
+    cy.loginAndSelectCluster();
+    cy.goToNamespaceDetails();
+  })
+
+    it('Uploads Service Instance and Service Bindings YAMLs', () => {
+      cy.contains('Upload YAML').click();
+  
+      cy.loadFiles(
+        'examples/service-binding-mock-crd.yaml',
+        'examples/service-instance-mock-crd.yaml',
+      ).then(resources => {
+        const input = resources.map(r => jsyaml.dump(r)).join('\n---\n');
+        cy.pasteToMonaco(input);
+      });
+  
+      cy.contains('Submit').click();
+  
+      cy.get('.fd-dialog__body')
+        .find('.sap-icon--message-success')
+        .should('have.length', 2);
+    });
+
+  describe('Check Service Management Extensions', () => {
+    before(()=>{
+      cy.loginAndSelectCluster();
+      cy.goToNamespaceDetails();
+    })
+    useCategory('Service Management');
+
+    it('Test Service Bindings', () => {
+      cy.checkExtension('Service Bindings');
+    });
+
+    it('Test Service Instances', () => {
+      cy.checkExtension('Service Instances');
+    });
+  });
+});
