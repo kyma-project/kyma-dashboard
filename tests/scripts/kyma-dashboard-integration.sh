1ce89ac5fbde3bf273a636cce8025d00d09f4210
mrCherry97 <mwisnia97@gmail.com>
2023-07-07 12:58:53 +0200

    fix screenShot and video dir, command to run tests

--- tests/scripts/kyma-dashboard-integration.sh
+++ tests/scripts/kyma-dashboard-integration.sh
@@ -33,4 +33,4 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' "$CYPRESS_DOMAIN")" != "200
 sleep 10
 
 cd tests
-npm ci && cypress run --browser chrome -C cypress.config.js
+npm ci && npm run test:e2e
