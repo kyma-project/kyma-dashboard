589a7c5b185a5ad5a0bad5cdfb1871ffbb09bd76
mrCherry97 <mwisnia97@gmail.com>
2023-08-02 22:12:19 +0200

    move from nkyma to k3d with modular kyma

--- tests/scripts/kyma-dashboard-integration.sh
+++ tests/scripts/kyma-dashboard-integration.sh
@@ -6,26 +6,33 @@ export NO_COLOR=1
 export KUBECONFIG="$GARDENER_KYMA_PROW_KUBECONFIG"
 export REPO_IMG_DEV="k3d-registry.localhost:5000/kyma-dashboard"
 export TAG="test-dev"
+OS="$(uname -s)"
+ARCH="$(uname -m)"
 
-apt-get update -y 
-apt-get install -y gettext-base
+curl -sSLo kyma.tar.gz "https://github.com/kyma-project/cli/releases/latest/download/kyma_${OS}_${ARCH}.tar.gz"
+tar -zxvf kyma.tar.gz
+chmod +x ./kyma
 
+echo "Provisioning k3d cluster for Kyma"
+./kyma provision k3d --ci
+
+./kyma alpha deploy
+
+echo "Apply and enable serverless module"
+kubectl apply -f https://github.com/kyma-project/serverless-manager/releases/latest/download/moduletemplate.yaml
+./kyma alpha enable module serverless -c fast
+
+echo "Apply istio module"
+kubectl apply -f https://github.com/kyma-project/istio/releases/latest/download/istio-manager.yaml
+kubectl apply -f https://github.com/kyma-project/istio/releases/latest/download/istio-default-cr.yaml
+
+echo "Create k3d registry..."
 k3d registry create registry.localhost --port=5000
 
-cat << EOF | kubectl create -f - --raw "/apis/core.gardener.cloud/v1beta1/namespaces/garden-kyma-prow/shoots/nkyma/adminkubeconfig" | jq -r ".status.kubeconfig" | base64 -d > "kubeconfig--kyma--nkyma.yaml"
-{
-    "apiVersion": "authentication.gardener.cloud/v1alpha1",
-    "kind": "AdminKubeconfigRequest",
-    "spec": {
-    "expirationSeconds": 10800
-    }
-}
-EOF
-
-cp kubeconfig--kyma--nkyma.yaml tests/fixtures/kubeconfig.yaml
-
+echo "Make release-dev..."
 make release-dev
 
+echo "Running kyma-dashboard..."
 docker run -d --rm --net=host --pid=host --name kyma-dashboard "$REPO_IMG_DEV-local-dev:$TAG"
 
 echo "waiting for server to be up..."
