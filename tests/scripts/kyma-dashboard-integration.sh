744e377e95cd388298ffcfa4fc2a446a76aa153e
mrCherry97 <mwisnia97@gmail.com>
2023-08-03 12:39:01 +0200

    remove exits

--- tests/scripts/kyma-dashboard-integration.sh
+++ tests/scripts/kyma-dashboard-integration.sh
@@ -31,8 +31,6 @@ echo "Apply gardener and service menegment resources"
 kubectl apply -f /tests/fixtures/examples
 
 k3d kubeconfig get kyma > tests/fixtures/kubeconfig.yaml
-
-exit 1
 }
 
 function busild_and_run_busola() {
@@ -48,8 +46,6 @@ docker run -d --rm --net=host --pid=host --name kyma-dashboard "$REPO_IMG_DEV-lo
 echo "waiting for server to be up..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' "$CYPRESS_DOMAIN")" != "200" ]]; do sleep 5; done
 sleep 10
-
-exit 1
 }
 
 deploy_k3d_kyma  &> $ARTIFACTS/kyma-alpha-deploy.log &
