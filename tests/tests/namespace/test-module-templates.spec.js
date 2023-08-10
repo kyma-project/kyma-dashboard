1e353480660e5748d0941f488f1f9cf6498a6904
Julius-Schmidt <schmidt-jvlius@gmx.de>
2023-08-10 10:41:34 +0200

    added tests module templates

---
+++ tests/tests/namespace/test-module-templates.spec.js
@@ -0,0 +1,40 @@
+const ISSUER_NAME = `test-issuer-${Math.floor(Math.random() * 9999) + 1000}`;
+const SECRET_NAME = `issuer-secret-${Math.floor(Math.random() * 9999) + 1000}`;
+
+context('Test Module-templates', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.loginAndSelectCluster();
+    cy.getLeftNav()
+      .contains('Namespaces')
+      .click();
+
+    cy.get('[role=row]')
+      .contains('kyma-system')
+      .click();
+
+    cy.navigateTo('Kyma', 'Module Templates');
+
+    cy.contains('keda-fast').click();
+
+    cy.url().should('match', new RegExp(`/moduletemplates/keda-fast`));
+  });
+
+  it('Inspect Module-templates', () => {
+    cy.contains('argocd.argoproj.io/instance=kyma-modules-fast-channel');
+    cy.contains('operator.kyma-project.io/managed-by=lifecycle-manager');
+    cy.contains('operator.kyma-project.io/controller-name=manifest');
+    cy.contains('operator.kyma-project.io/module-name=keda');
+    cy.contains('Data');
+    cy.contains('Descriptor');
+
+    cy.get('[class="fd-object-status__text"]:visible').contains('fast');
+
+    cy.contains('View YAML').click();
+
+    cy.contains('keda-fast.yaml');
+
+    cy.get('[aria-label="close drawer"]:visible').click();
+  });
+});
