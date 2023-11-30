c560d230237b2c37e4ce2e1b14cd8873c04a1229
Agata Kucharska <agata.kucharska@sap.com>
2023-11-30 15:07:50 +0100

    Will it work?

--- tests/support/helpers.js
+++ tests/support/helpers.js
@@ -2,7 +2,7 @@ export function chooseComboboxOption(selector, optionText) {
   cy.get(`ui5-combobox${selector}:visible`)
     .find('input')
     .filterWithNoValue()
-    .click()
+    .click({ force: true })
     .type(optionText, { force: true });
 
   cy.get('ui5-li:visible', { timeout: 10000 })
