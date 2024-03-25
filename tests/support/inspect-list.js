c1a5048b42b6845495a8e4383235281b9594c3e8
Agata Kucharska <agata.kucharska@sap.com>
2024-03-25 09:12:15 +0100

    Fix run before, add commands, fix authorisation policies

--- tests/support/inspect-list.js
+++ tests/support/inspect-list.js
@@ -1,10 +1,11 @@
 Cypress.Commands.add('inspectList', (resource, resourceName) => {
   cy.closeMidColumn();
   cy.wait(1000);
-  cy.get('ui5-combobox[placeholder="Search"]:visible:visible')
-    .find('input')
-    .click()
-    .type(resourceName);
 
-  cy.contains(resourceName).should('be.visible');
+  cy.get('ui5-input[placeholder="Search"]:visible')
+    .find('input')
+    .wait(1000)
+    .type(`${resourceName}{enter}`);
+
+  cy.contains('span', resourceName).should('be.visible');
 });
