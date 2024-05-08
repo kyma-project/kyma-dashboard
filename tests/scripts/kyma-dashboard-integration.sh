a88ccf0e8868654245686b5a8ed5dd0a1873bf34
Piotr Halama <skrzynka@halamix2.pl>
2024-05-08 11:08:34 +0200

    explain why we need internal registry

--- tests/scripts/kyma-dashboard-integration.sh
+++ tests/scripts/kyma-dashboard-integration.sh
@@ -21,12 +21,9 @@ chmod +x ./kyma
 echo "Provisioning k3d cluster for Kyma"
 k3d registry create kyma-registry --port 5001
 
+# kyma alpha deploy command expects a cluster with an internal k3d registry, so we provide one
 k3d cluster create kyma --kubeconfig-switch-context -p 80:80@loadbalancer -p 443:443@loadbalancer --registry-use kyma-registry
 kubectl create ns kyma-system
-
-
-# uncomment after command fix
-# ./kyma deploy
 
 ./kyma alpha deploy
 
