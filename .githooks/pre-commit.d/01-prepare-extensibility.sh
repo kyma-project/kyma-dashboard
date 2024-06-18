ab3ac97a2ec2139afae8ee10c78c039f37ef806b
mrCherry97 <mwisnia97@gmail.com>
2024-06-18 11:55:47 +0200

    change image to trigger githook

--- .githooks/pre-commit.d/01-prepare-extensibility.sh
+++ .githooks/pre-commit.d/01-prepare-extensibility.sh
@@ -6,7 +6,7 @@ echo "Running extensions pre-commit hook..."
   cd "$(git rev-parse --show-toplevel)" || exit 1
     for environment in "${environments[@]}"; do
       # shellcheck disable=SC2086
-      modified=$(git diff --exit-code --name-only environments/${environment}) 
+      modified=$(git diff --staged --exit-code --name-only environments/${environment}) 
       if [ -n "$modified" ]; then
         echo "Changes detected in ${environment} environment. Rendering extensibility..."
         ENV=${environment} npm run prepare-extensions
@@ -15,7 +15,7 @@ echo "Running extensions pre-commit hook..."
         ENV=${environment} npm run pack-wizards
         ENV=${environment} npm run prepare-statics
         ENV=${environment} npm run pack-statics
-        git add -u $files
+        git add environments/${environment}
         git commit -m "chore: update extensibility for ${environment} environment"
         else
           echo "No changes in ${environment} environment. Skipping.."
