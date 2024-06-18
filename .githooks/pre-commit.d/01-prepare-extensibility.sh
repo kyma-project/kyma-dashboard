1e3852da432fb9d39b28bd3c3c21a89502b5a2e2
mrCherry97 <mwisnia97@gmail.com>
2024-06-18 11:59:56 +0200

    remove unused comment

--- .githooks/pre-commit.d/01-prepare-extensibility.sh
+++ .githooks/pre-commit.d/01-prepare-extensibility.sh
@@ -5,7 +5,6 @@ echo "Running extensions pre-commit hook..."
 (
   cd "$(git rev-parse --show-toplevel)" || exit 1
     for environment in "${environments[@]}"; do
-      # shellcheck disable=SC2086
       modified=$(git diff --staged --exit-code --name-only environments/${environment}) 
       if [ -n "$modified" ]; then
         echo "Changes detected in ${environment} environment. Rendering extensibility..."
