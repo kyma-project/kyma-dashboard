79fcdea832cb6fec5af6e27f5a91df1b93395423
mrCherry97 <mwisnia97@gmail.com>
2024-06-18 10:45:05 +0200

    feat: add githooks for extensions, wizards and statics

---
+++ .githooks/pre-commit.d/01-prepare-extensibility.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+environments=(dev stage prod)
+echo "Running extensions pre-commit hook..."
+(
+  cd "$(git rev-parse --show-toplevel)" || exit 1
+    for environment in "${environments[@]}"; do
+      # shellcheck disable=SC2086
+      modified=$(git diff --exit-code --name-only environments/${environment}) 
+      if [ -n "$modified" ]; then
+        echo "Changes detected in ${environment} environment. Rendering extensibility..."
+        ENV=${environment} npm run prepare-extensions
+        ENV=${environment} npm run pack-extensions
+        ENV=${environment} npm run prepare-wizards
+        ENV=${environment} npm run pack-wizards
+        ENV=${environment} npm run prepare-statics
+        ENV=${environment} npm run pack-statics
+        else
+          echo "No changes in ${environment} environment. Skipping.."
+      fi
+    done
+)
