84a383ba04d46a95097f21e14ea7f189a4966e33
mrCherry97 <mwisnia97@gmail.com>
2024-06-19 13:58:07 +0200

    rename

--- .githooks/pre-commit.d/01-prepare-extensibility.sh
+++ .githooks/pre-commit.d/01-prepare-extensibility.sh
@@ -8,7 +8,7 @@ echo "Running extensions pre-commit hook..."
       modified=$(git diff --staged --exit-code --name-only environments/${environment} extensions)
       if [ -n "$modified" ]; then
         echo "Changes detected in ${environment} environment. Rendering extensibility..."
-        npm run prepare-${environment}-extensibility
+        npm run prepare-extensibility-${environment}
         git add environments/${environment}
         else
           echo "No changes in ${environment} environment. Skipping.."
