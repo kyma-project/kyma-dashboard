3d361cbe4fd9f491b3446e0058921f2e67895f2c
mrCherry97 <mwisnia97@gmail.com>
2024-06-19 15:41:42 +0200

    test this shit

--- environments/dev/env.sh
+++ environments/dev/env.sh
@@ -2,6 +2,4 @@ export WEB_IMAGE=v20240618-0bf0ff64
 export BACKEND_IMAGE=v20240118-9e254ef3
 export LOCAL_IMAGE=v20240618-0bf0ff64
 echo "Environment variables set for dev"
-export echo(WEB_IMAGE=v20240618-0bf0ff64)
-export echo(BACKEND_IMAGE=v20240118-9e254ef3)
-export echo(LOCAL_IMAGE=v20240618-0bf0ff64)
+make $*
