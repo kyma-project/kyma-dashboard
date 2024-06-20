ea4771a52a44c24d6d6ebc9c1f977565f880b2f6
Damian Badura <damian.badura@sap.com>
2024-06-20 16:31:27 +0200

    wip

--- .github/scripts/check-extensions.sh
+++ .github/scripts/check-extensions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-git diff --name-only environments/dev extensions
+>&2 git diff --name-only environments/dev extensions
 modified=$(git diff --name-only environments/dev extensions)
 if [ -n "$modified" ]; then
-  echo "Changes detected in dev environment. Rendering extensibility..."
+  >&2 echo "Changes detected in dev environment. Rendering extensibility..."
   echo "CHANGED=true"
-  else
-    echo "No changes in ${environment} environment. Skipping.."
-    echo "CHANGED=false"
+else
+  >&2 echo "No changes in ${environment} environment. Skipping.."
+  echo "CHANGED=false"
 fi
